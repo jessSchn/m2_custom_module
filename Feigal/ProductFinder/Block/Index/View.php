44dd3d98c82da2c2752e9ed4fa716b2b32cb7cb2
Jessica Fegial <jessica.feigal@irishtitan.com>
2020-08-05 10:04:26 -0500

    updateds from Kevin

--- Feigal/ProductFinder/Block/Index/View.php
+++ Feigal/ProductFinder/Block/Index/View.php
@@ -1,49 +1,63 @@
 <?php
-    /*
-     * PartFinder Block
-     * Created by Jessica Feigal <mrs.feigal@gmail.com>
-     * Date: 7/29/2020
+/*
+ * PartFinder Block
+ * Created by Jessica Feigal <mrs.feigal@gmail.com>
+ * Date: 7/29/2020
+ */
+
+namespace Feigal\ProductFinder\Block\Index;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollection;
+use Magento\CatalogInventory\Model\Stock\StockItemRepository;
+
+class View extends Template
+{
+    protected $stock_item;
+    protected $product_collection;
+
+    public function __construct(
+        ProductCollection $product_collection,
+        StockItemRepository $stock_item,
+        Context $context,
+        array $data = []
+    ) {
+        $this->stock_item = $stock_item;
+        $this->product_collection = $product_collection;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return $prodcuts
      */
-
-    namespace Feigal\ProductFinder\Block\Index;
-     
-    use Magento\Framework\View\Element\Template;
-    use Magento\Framework\View\Element\Template\Context;
-    use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollection;
-     
-    class View extends Template
+    public function getProducts()
     {
-        
-        protected $product_collection;
-    
-        public function __construct(
-            ProductCollection $product_collection
-            Context $context,
-            array $data = []
-        ) {
-            $this->product_collection = $product_collection;
-            parent::__construct($context, $data);
-        }
+        $data = $this->getRequest()->getParams();
+        try {
+            $products = $this->product_collection->create()
+                             ->addAttributeToFilter('price', ['gteq' => $data['lowprice']])
+                             ->addAttributeToFilter('price', ['lteq' => $data['highprice']])
+                             ->setOrder('price', $data['order'])
+                             ->setPageSize(10);
 
-        /**
-         * @return $prodcuts
-         */
-        public function getProducts($data)
-        {
-            try 
-                $products = $this->product_collection->create()
-                            ->addAttributeToFilter('price', [
-                                    ['gteq' => $data['lowprice'], ['lteq' => $data['highprice']]
-                                ]
-                            )
-                            ->order('price', $data['order'])
-                            ->setPageSize(10);
-                
-            } catch (\Exception $e) {
-                return false;
-            }
-            return $products;
+        } catch (\Exception $e) {
+            return false;
+        }
+        return $products;
+    }
+
+    public function getStock($pid)
+    {
+        try {
+            $stock = $this->stock_item->get($pid);
+            return $stock->getQty();
+        }
+        catch(\Exception $e) {
+            return 0;
         }
 
     }
+
+}
     
--- Feigal/ProductFinder/view/frontend/web/js/productfinder.js
+++ Feigal/ProductFinder/view/frontend/web/js/productfinder.js
@@ -8,36 +8,36 @@
 define([
     "jquery",
     "jquery/ui"
-], function($){
+], function ($) {
     "use strict";
 
     function main(config, element) {
         var $element = $(element);
         var AjaxUrl = config.AjaxUrl;
-         
+
         var dataForm = $('#prod-finder-form');
         dataForm.mage('validation', {});
-         
-        $(document).on('click','.submit',function() {
-             
-            if (dataForm.valid()){
-                event.preventDefault();
-                var param = dataForm.serialize();
-                alert(param);
-                    $.ajax({
-                        context:'#product-results',
-                        showLoader: true,
-                        url: AjaxUrl,
-                        data: param,
-                        type: "POST"
-                    }).done(function (data) {
-                        $('#produt-results').html(data);
 
-                        return true;
-                    });
-                }
-            });
+        $(document).on('click', '.submit', function () {
+
+            if (dataForm.valid()) {
+                event.preventDefault();
+                let param = dataForm.serialize();
+
+                $.ajax({
+                    context: '#product-results',
+                    showLoader: true,
+                    url: AjaxUrl,
+                    data: param,
+                    type: "POST"
+                }).done(function (data) {
+                    $('#product-results').html(data.output);
+
+                    return true;
+                });
+            }
+        });
     };
 
-    return main;  
+    return main;
 });
